efffe045ebddd8744ff08ef50ba14b24a44fa29e
Souravdhar7595 <148620289+Souravdhar7595@users.noreply.github.com>
2024-02-09 13:31:55 +0530

    first commit

---
+++ qusetion1/index.js
@@ -0,0 +1,7 @@
+let par = document.getElementById('text')
+function js_style(){
+    par.style.color ="red";
+    par.style.textAlign = "center";
+    par.style.fontSize ="40px"
+    par.innerHTML ="hello my name is sourav dhar"
+}
---
+++ qusetion3/index.js
@@ -0,0 +1,8 @@
+function save(){
+    let inp = document.getElementById("input1");
+    inp.style.color="red";
+    inp.style.backgroundColor ="blue";
+    let pa=document.getElementsByTagName("p");
+    pa.style.backgroundColor="green"
+    pa.style.color="blue"
+}
